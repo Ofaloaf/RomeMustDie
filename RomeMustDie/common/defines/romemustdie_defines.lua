df99e214c9581538ea873fc37d4634cbae09f544
Ofaloaf <mkundakco08@charter.net>
2018-01-04 10:45:02 -0500

    2017 updates
    I'm doing it, I'm making it happen

---
+++ RomeMustDie/common/defines/romemustdie_defines.lua
@@ -0,0 +1,9 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.0
+
+NDefines.NTitle.ALLOW_DE_JURE_ASSIMILATION_ANYWHERE = 0
+
+NDefines.NTechnology.DONT_EXECUTE_TECH_BEFORE = 475
+NDefines.NTechnology.IDEAL_YEAR_LEVEL_0 = 400 					-- the chance to get a progress to this level will increase after this date and decrease before it
+NDefines.NTechnology.IDEAL_YEAR_LEVEL_8 = 1200
